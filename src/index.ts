25288f44ee053d0f05fd75332133adc13a403246
100NikhilBro <100pranjalgupta@gmail.com>
2026-01-25 15:13:10 +0530

    preparing fir relesed

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
+// import { HiSecure } from "./core/HiSecure.js";
+// import { useSecure, secureRoute } from "./core/useSecure.js";
+
+
+// export { z } from "zod";
+// export { body, query, param, header } from "express-validator";
+
+// const hiSecure = HiSecure.getInstance();
+
+// export { 
+//     HiSecure,        
+//     hiSecure,        
+//     useSecure,       
+//     secureRoute      
+// };
+
+// export default hiSecure;
+
+
+
+
 import { HiSecure } from "./core/HiSecure.js";
 import { useSecure, secureRoute } from "./core/useSecure.js";
-
 
 export { z } from "zod";
 export { body, query, param, header } from "express-validator";
 
-const hiSecure = HiSecure.getInstance();
 
-export { 
-    HiSecure,        
-    hiSecure,        
-    useSecure,       
-    secureRoute      
+export {
+  HiSecure,
+  useSecure,
+  secureRoute
 };
-
-export default hiSecure;
-
-
 
